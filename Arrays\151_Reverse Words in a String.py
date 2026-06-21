64d41f2ec5b29d08fbe1a48b0d68d417f4610b85
Lakshanika MSB <lakshanikamsb@gmail.com>
2026-06-21 22:02:43 +0530

    Implement reverseWords method to reverse string words

---
+++ Arrays\151_Reverse Words in a String.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words=s.split()
+        ans=""
+        for i in range(len(words)-1,-1,-1):
+            ans+=words[i]
+            if i!=0:
+                ans+=" "
+        return ans
